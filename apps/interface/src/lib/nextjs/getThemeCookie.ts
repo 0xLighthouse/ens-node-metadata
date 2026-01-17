b649df8110887a91e38f3afb845b462fb2bbc572
1a35e1 <1a35e1@protonmail.com>
2026-01-17 20:56:16 +0000

    feat: Drop interface

--- apps/interface/src/lib/nextjs/getThemeCookie.ts
+++
@@ -1,7 +0,0 @@
-import { UITheme } from "@/config/theme"
-import { cookies } from "next/headers"
-
-// Available SSR only
-export const getThemeCookie = () => {
-  return cookies().get('theme')?.value as UITheme || UITheme.LIGHT
-}
--- apps/interface/src/lib/resolveName.ts
+++
@@ -1,6 +0,0 @@
-import { shortAddress } from './utils'
-
-// TODO: Resolve ENS name
-export const resolveName = async (address: string) => {
-  return shortAddress(address)
-}
