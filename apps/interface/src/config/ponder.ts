d9f24319b5b2fbc0b5feb046db0a31e601c94a04
1a35e1 <1a35e1@protonmail.com>
2026-02-17 17:51:36 +0000

    chore: Drop unused code

--- apps/interface/src/config/ponder.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@ponder/client'
-import * as ponderSchema from '../../../indexers/ponder.schema'
-
-// https://signals-production-6591.up.railway.app/
-const ENDPOINT = 'https://signals-production-6591.up.railway.app'
-
-export const schema = ponderSchema
-export const client = createClient(`${ENDPOINT}/sql`, { schema })
