8d7fa21fd44d840c042888bb5576301dd8a66acd
1a35e1 <1a35e1@protonmail.com>
2026-02-25 22:49:26 +0000

    feat: Fix build

---
+++ packages/agent/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    cli: 'src/cli.ts',
+    index: 'src/index.ts',
+  },
+  format: ['esm'],
+  target: 'node18',
+  dts: true,
+  clean: true,
+  noExternal: ['@ens-node-metadata/shared'],
+})
